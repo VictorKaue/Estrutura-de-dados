61a2d4235db062805a245817193ded5bd2612054
VictorKaue <victorkaue909@gmail.com>
2024-09-25 08:49:46 -0300

    att

--- Ordenadores/bubble_sort.c
+++ Ordenadores/bubble_sort.c
@@ -1,26 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <locale.h>
 
-#define tamanho 6
-
-void imprimir_vetor(int *vetor){
+void imprimir_vetor(int *vetor, int tamanho){
     int i;
     for(i = 0; i < tamanho; i++){
         printf("%dª posição: %d\n", i+1, vetor[i]);
     }
 }
 
-int main(){
-    srand(time(NULL));
-    int vetor[tamanho], i, j, aux;
-    
-    for (i = 0; i < tamanho-1; i++){
-        vetor[i] = rand() %10 + 1;
-    }
-    printf("Vetor antes: \n\n");
-    imprimir_vetor(vetor);
-
+void bubblesort(int *vetor, int tamanho){
+    int i,j,aux;
     for (i = 0; i < tamanho - 1; i++){
         for (j = 0; j < tamanho-1-i; j++){
             if (vetor[j] > vetor[j+1]){
@@ -30,6 +21,20 @@ int main(){
             }
         }
     }
-    printf("Vetor depois: \n\n");
-    imprimir_vetor(vetor);
+}
+
+int main(){
+	setlocale(LC_ALL, "Portuguese");
+    srand(time(NULL));
+    int vetor[6], i;
+    int tamanho = sizeof(vetor)/sizeof(vetor[0]);
+
+    for (i = 0; i < tamanho-1; i++){
+        vetor[i] = rand() %100 + 1;
+    }
+    printf("Vetor antes: \n\n");
+    imprimir_vetor(vetor, tamanho);
+	bubblesort(vetor, tamanho);
+    printf("\nVetor depois: \n\n");
+    imprimir_vetor(vetor, tamanho);
 }
--- Ordenadores/insertion_sort.c
+++ Ordenadores/insertion_sort.c
@@ -1,31 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <locale.h>
 
-#define tamanho 6
-
-void imprimir_vetor(int *vetor){
+void imprimir_vetor(int *vetor, int tamanho){
     int i;
     for(i = 0; i < tamanho; i++){
         printf("%dª posição: %d\n", i+1, vetor[i]);
     }
 }
 
-int main(){
-    srand(time(NULL));
-    int vetor[tamanho], i, j, menor_posicao;
-    for (i = 0; i < tamanho-1; i++){
-        vetor[i] = rand() %10 + 1;
-    }
-    
-    for (i = 0; i < tamanho-1; i++){
-        menor_posicao = vetor[i];
-        for (j = 0; j < tamanho-1-i; j++){
-            if(menor_posicao < vetor[i]){
-                
-            }
+void insertionsort(int vetor, int tamanho){
+    int i, j, aux;
+
+    for(i = 0; i < tamanho-1; i++){
+        for(j = i+1; j < tamanho; j++){
+            
         }
     }
-     
-    return 0;
+}
+
+int main(){
+	setlocale(LC_ALL, "Portuguese");
+    srand(time(NULL));
+    int vetor[6], i;
+    int tamanho = sizeof(vetor)/sizeof(vetor[0]);
+
+    for (i = 0; i < tamanho-1; i++){
+        vetor[i] = rand() %100 + 1;
+    }
+    printf("Vetor antes: \n\n");
+    imprimir_vetor(vetor, tamanho);
+	insertionsort(vetor, tamanho);
+    printf("\nVetor depois: \n\n");
+    imprimir_vetor(vetor, tamanho);
 }
